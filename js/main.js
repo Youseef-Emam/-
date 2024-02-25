1618c2e713f583a590d0112385bbe8fd0fd4abbd
Youseef EMAM <152444249+Youseef-Emam@users.noreply.github.com>
2024-02-25 03:26:30 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("btn");
+const link = document.getElementById("link");
+const last = document.getElementById("last");
+const go = document.getElementById("go");
+
